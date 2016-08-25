679edda35b6828b4eeae9f40807dff5620b02979
Kostas Vlyssidis <k.vlyssidis@sig.eu>
2016-08-25 16:27:21 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+app.listen(8080,()=>{
+	console.info("Server started at: " + new Date());
+})
+
+/*
+The path that you provide to the express.static function is relative to the directory from where you launch your node process. If you run the express app from another directory, it’s safer to use the absolute path of the directory that you want to serve.
+*/
+app.use("/static",express.static('public'));
