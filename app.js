be9bf41164f4e7c3b825247b0050aebef648e6cd
Kostas Vlyssidis <k.vlyssidis@sig.eu>
2016-08-25 20:26:16 +0300

    Setup route for home page, remove /static path prefix and create template and controller for home page

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.listen(8080,()=>{
 /*
 The path that you provide to the express.static function is relative to the directory from where you launch your node process. If you run the express app from another directory, it’s safer to use the absolute path of the directory that you want to serve.
 */
-app.use("/static",express.static('public'));
+app.use(express.static('public'));
