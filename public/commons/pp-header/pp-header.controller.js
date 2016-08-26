34d6ec3f77c9b10a55c2fdff0c87cc51b1229add
Kostas Vlyssidis <k.vlyssidis@sig.eu>
2016-08-26 15:46:34 +0300

    Create layout and header component

---
+++ public/commons/pp-header/pp-header.controller.js
@@ -0,0 +1,19 @@
+(function(){
+	angular.module('personal-portal').controller('HeaderController',HeaderController);
+
+	HeaderController.$inject=[];
+
+	function HeaderController(){
+		var ctrl = this;
+		ctrl.name = undefined;
+
+
+		ctrl.$onInit = initializeController();
+
+		function initializeController(){
+			//TODO replace with call to backend
+			ctrl.name = "Konstantinos Vlyssidis";
+		}
+
+	}
+})();
